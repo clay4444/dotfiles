bb539f3cc96fce16518ed3fcf0a79ff7a82a8335
loushang.ls <loushang.ls@antgroup.com>
2024-01-31 20:06:08 +0800

    Config bufferline

--- nvim/lua/plugins/bufferline.lua
+++ nvim/lua/plugins/bufferline.lua
@@ -7,38 +7,38 @@ end
 
 return {
   "akinsho/bufferline.nvim",
-  dependencies = { "nvim-tree/nvim-web-devicons" },
+  dependencies = { "nvim-tree/nvim-web-devicons", "moll/vim-bbye" },
   version = "*",
-  -- options for bufferline. This will automatically call `require("bufferline").setup(opts)`
-  opts = {
-    -- https://github.com/akinsho/bufferline.nvim#configuration
-    options = {
-      -- Use moll/vim-bbye :Bdelete command to close the tab.
-      close_command = "Bdelete! %d",
-      right_mouse_command = "Bdelete! %d",
-      -- Sidebar(nvim-tree) configuration.
-      offsets = {
-        {
-          filetype = "NvimTree",
-          text = "File Explorer",
-          highlight = "Directory",
-          text_align = "left",
-        },
-      },
-      -- Use LSP.
-      diagnostics = "nvim_lsp",
-      ---@diagnostic disable-next-line: unused-local
-      diagnostics_indicator = function(count, level, diagnostics_dict, context)
-        local s = " "
-        for e, n in pairs(diagnostics_dict) do
-          local sym = e == "error" and " " or (e == "warning" and " " or "")
-          s = s .. n .. sym
-        end
-        return s
-      end,
-    },
-  },
   config = function()
+    -- https://github.com/akinsho/bufferline.nvim#configuration
+    require("bufferline").setup({
+      options = {
+        -- Use moll/vim-bbye :Bdelete command to close the tab.
+        close_command = "Bdelete! %d",
+        right_mouse_command = "Bdelete! %d",
+        -- Sidebar(nvim-tree) configuration.
+        offsets = {
+          {
+            filetype = "NvimTree",
+            text = "File Explorer",
+            highlight = "Directory",
+            text_align = "left",
+          },
+        },
+        -- Use LSP.
+        diagnostics = "nvim_lsp",
+        ---@diagnostic disable-next-line: unused-local
+        diagnostics_indicator = function(count, level, diagnostics_dict, context)
+          local s = " "
+          for e, n in pairs(diagnostics_dict) do
+            local sym = e == "error" and " " or (e == "warning" and " " or "")
+            s = s .. n .. sym
+          end
+          return s
+        end,
+      },
+    })
+
     -- Keybindings.
     keymap("n", uBufferLine.prev, ":BufferLineCyclePrev<CR>")
     keymap("n", uBufferLine.next, ":BufferLineCycleNext<CR>")
