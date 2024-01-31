72843802c588b0d4cfa0542c0d08f23b4b17b430
loushang.ls <loushang.ls@antgroup.com>
2024-01-31 22:57:57 +0800

    Re-config nvimtree

--- nvim/lua/plugins/nvim-tree.lua
+++ nvim/lua/plugins/nvim-tree.lua
@@ -16,46 +16,89 @@ return {
     vim.g.loaded_netrw = 1
     vim.g.loaded_netrwPlugin = 1
 
-    -- change color for arrows in tree to light blue
-    vim.cmd([[ highlight NvimTreeFolderArrowClosed guifg=#3FC5FF ]])
-    vim.cmd([[ highlight NvimTreeFolderArrowOpen guifg=#3FC5FF ]])
+    -- Custom Mappings
+    local function my_on_attach(bufnr)
+      local api = require "nvim-tree.api"
 
-    -- configure nvim-tree
+      local function opts(desc)
+        return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+      end
+
+      -- default mappings
+      api.config.mappings.default_on_attach(bufnr)
+
+      -- custom mappings
+      keymap("n", uTree.edit,                   api.node.open.edit,                    opts('Up'))
+      keymap("n", uTree.vsplit,                 api.node.open.vertical,                opts('Up'))
+      keymap("n", uTree.split,                  api.node.open.horizontal,              opts('Up'))
+      keymap("n", uTree.toggle_git_ignored,     api.tree.toggle_gitignore_filter,      opts('Up'))
+      keymap("n", uTree.toggle_dotfiles,        api.tree.toggle_hidden_filter,         opts('Up'))
+      keymap("n", uTree.toggle_custom,          api.tree.toggle_custom_filter,         opts('Up'))
+      keymap("n", uTree.refresh,                api.tree.reload,                       opts('Up'))
+      keymap("n", uTree.create,                 api.fs.create,                         opts('Up'))
+      keymap("n", uTree.remove,                 api.fs.remove,                         opts('Up'))
+      keymap("n", uTree.rename,                 api.fs.rename,                         opts('Up'))
+      keymap("n", uTree.copy,                   api.fs.copy.node,                      opts('Up'))
+      keymap("n", uTree.cut,                    api.fs.cut,                            opts('Up'))
+      keymap("n", uTree.paste,                  api.fs.paste,                          opts('Up'))
+      keymap("n", uTree.copy_name,              api.fs.copy.filename,                  opts('Up'))
+      keymap("n", uTree.copy_path,              api.fs.copy.relative_path,             opts('Up'))
+      keymap("n", uTree.copy_absolute_path,     api.fs.copy.absolute_path,             opts('Up'))
+      keymap("n", uTree.toggle_file_info,       api.node.show_info_popup,              opts('Up'))
+      keymap("n", uTree.tabnew,                 api.node.open.tab,                     opts('Up'))
+      keymap("n", uTree.cd,                     api.tree.change_root_to_node,          opts('Up'))
+      keymap("n", uTree.dir_up,                 api.node.navigate.parent,              opts('Up'))
+    end
+
     nvimtree.setup({
-      view = {
-        width = 35,
-        relativenumber = true,
+      disable_netrw = true,
+      git = {
+          enable = true,
+          ignore = true,
       },
-      -- change folder arrow icons
+      -- Support project plugin.
+      update_cwd = false,
+      update_focused_file = {
+          enable = true,
+          update_cwd = false,
+      },
+      -- Hide dotfiles and customer dir.
+      filters = {
+          dotfiles = true,
+          custom = { },
+      },
+      view = {
+          width = 40,
+          -- or right.
+          side = 'left',
+          number = false,
+          relativenumber = false,
+          signcolumn = 'yes',
+      },
+      actions = {
+          open_file = {
+              resize_window = true,
+              quit_on_open = true,
+          },
+      },
+      system_open = {
+          cmd = 'open',
+      },
       renderer = {
         indent_markers = {
-          enable = true,
+          enable = false,
+          icons = {
+            corner = "└ ",
+            edge = "│ ",
+            none = "  ",
+          },
         },
         icons = {
-          glyphs = {
-            folder = {
-              arrow_closed = "", -- arrow when folder is closed
-              arrow_open = "", -- arrow when folder is open
-            },
-          },
+          webdev_colors = true,
+          git_placement = "after",
         },
       },
-      -- disable window_picker for
-      -- explorer to work well with
-      -- window splits
-      actions = {
-        open_file = {
-          window_picker = {
-            enable = false,
-          },
-        },
-      },
-      filters = {
-        custom = { ".DS_Store" },
-      },
-      git = {
-        ignore = false,
-      },
+      on_attach = my_on_attach,
     })
 
     -- set keymaps
