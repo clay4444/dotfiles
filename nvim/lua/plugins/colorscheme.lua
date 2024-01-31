ebab7cc517a096e7676b5888c07a01d0e9af45e5
loushang.ls <loushang.ls@antgroup.com>
2024-01-31 19:59:22 +0800

    update color schema config way

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -2,11 +2,11 @@ return {
   {
     "folke/tokyonight.nvim",
     priority = 1000, -- make sure to load this before all the other start plugins
-    opts = {
-      style = "storm", -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
-      transparent = false -- Enable this to disable setting the background color
-    },
     config = function()
+      require("tokyonight").setup({
+        style = "storm", -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
+        transparent = false -- Enable this to disable setting the background color
+      })
       -- load the colorscheme here
       vim.cmd([[colorscheme tokyonight]])
     end,
